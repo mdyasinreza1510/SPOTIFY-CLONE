f2f7d33673267bb6d10785a2fdd4b4109e5571c1
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-09-19 00:08:21 +0530

    done uploading music feature

--- BACKEND/src/services/storage.service.js
+++ BACKEND/src/services/storage.service.js
@@ -3,3 +3,15 @@ const {ImageKit}= require ("@ImageKit/nodejs")
 const ImageKitClient = new ImageKit({
     privateKey:process.env.IMAGEKIT_PRIVATE_KEY
 })
+
+async function uploadfile(file){
+    const result= await ImageKitClient.files.upload({
+        file,
+        fileName:"music_" + Date.now(),
+        folder:"spotify-clone/music"
+
+    })
+    return result;
+}
+
+module.exports ={uploadfile}
